b234a89b6fb32be9cffed2063aa10d47a2f76494
Maxxi <s0546567@htw-berlin.de>
2017-10-18 13:09:33 +0200

    Added update function for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
@@ -22,6 +25,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def article_params
       params.require(:article).permit(:title, :text)
